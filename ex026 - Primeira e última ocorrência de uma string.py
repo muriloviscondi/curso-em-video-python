dc26104663f81dc4509fda22692366ea3f1e44fc
muriloviscondi <muriloviscondi@gmail.com>
2020-05-01 14:10:15 -0300

    exercicio 026

---
+++ ex026 - Primeira e última ocorrência de uma string.py
@@ -0,0 +1,12 @@
+frase = str(input('Digite uma frase: ')).strip().upper()
+
+print(
+    'A letra A aparece {} vezes na frase.'
+    '\nA primeira letra A apareceu na posição {}'
+    '\nE a última letra A apareceu na posição {}'
+    .format(
+        frase.count('A'),
+        frase.find('A') + 1,
+        frase.rfind('A') + 1
+    )
+)
