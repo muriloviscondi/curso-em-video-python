a1519bbf5cde87e3c8f1bdf3d2634b01a11e5e6e
muriloviscondi <muriloviscondi@gmail.com>
2020-05-04 08:34:32 -0300

    exercicio 038

---
+++ ex038 - Comparando números.py
@@ -0,0 +1,10 @@
+n1 = int(input('Primeiro número:.. '))
+n2 = int(input('Segundo número:... '))
+
+if n1 != n2:
+    if n1 > n2:
+        print('O PRIMEIRO valor é MAIOR')
+    else:
+        print('O SEGUNDO valor é MAIOR')
+else:
+    print('Os DOIS valores são IGUAIS')
