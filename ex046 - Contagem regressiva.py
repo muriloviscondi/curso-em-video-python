cfeeb3cc00342f590247904cf0e02fbecee88438
muriloviscondi <muriloviscondi@gmail.com>
2020-05-07 23:09:11 -0300

    exercicio 046

---
+++ ex046 - Contagem regressiva.py
@@ -0,0 +1,18 @@
+from time import sleep
+
+for count in range(10, -1, -1):
+    print(count)
+    sleep(1)
+
+print('BOM! BOM! POOOW!')
+
+'''
+count = 10
+
+while count >= 0:
+    print(count)
+    sleep(1)
+    count -= 1
+
+print('BOM! BOM! POOOW!')
+'''
